90c2479b87b00a6cca8b9f0518c39cc3584f508f
Tom MacWright <tom@macwright.org>
2015-01-07 17:35:46 -0500

    Add JSDoc. Fixes #1

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // http://stackoverflow.com/questions/11935175/sampling-a-random-subset-from-an-array
-featureCollection = require('turf-featurecollection');
+var featureCollection = require('turf-featurecollection');
 
+/**
+ * Selects a given number of {@link Feature|features} from a {@link FeatureCollection}
+ * at random.
+ *
+ * @module turf/sample
+ * @param {FeatureCollection} features
+ * @param {number} n number of features to select
+ * @return {FeatureCollection} output
+ * @example
+ * var pts = JSON.parse(fs.readFileSync('/path/to/pts.geojson'))
+ * var num = 10
+ * var sampled = turf.sample(pts, num)
+ * console.log(sampled)
+ */
 module.exports = function(fc, num){
   var outFC = featureCollection(getRandomSubarray(fc.features, num));
   return outFC;
